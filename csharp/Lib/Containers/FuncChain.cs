d9978b2d1d0652438a77be22a529639ab42776ad
Ilya Seleznev <itsuart@gmail.com>
2015-07-14 18:14:17 +0600

    FuncChain

---
+++ csharp/Lib/Containers/FuncChain.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib.Containers
+{
+    public class FuncChain<T>
+    {
+        private readonly Func<T> _value;
+
+
+        public FuncChain(Func<T> value)
+        {
+            if (value == null) throw new ArgumentNullException("value");
+            _value = value;
+        } 
+
+        public FuncChain<T2> Fmap<T2>(Func<T, T2> f)
+        {
+            if (f == null) throw new ArgumentNullException("f");
+            return new FuncChain<T2>(() =>
+                {
+                    var forcedValue = Run();
+                    return f(forcedValue);
+                });
+        }
+ 
+        public FuncChain<T2> Seq<T2>(FuncChain<T2> next)
+        {
+            if (next == null) throw new ArgumentNullException();
+            return new FuncChain<T2>(() =>
+                {
+                    RunIgnoringResult();
+                    return next.Run();
+                });
+        } 
+
+        public FuncChain<T2> Bind<T2>(Func<T, FuncChain<T2>> f)
+        {
+            if (f == null) throw new ArgumentNullException("f");
+            return new FuncChain<T2>(() =>
+                {
+                    var forcedValue = Run();
+                    return f(forcedValue).Run();
+                });
+        } 
+
+        public T Run()
+        {
+            return _value();
+        }
+
+        public void RunIgnoringResult()
+        {
+            _value();
+        }
+
+        public static FuncChain<T> Wrap(T value)
+        {
+            return new FuncChain<T>(() => value);
+        } 
+    }
+}
---
+++ csharp/Tests/Containers/FuncChainTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lib.Containers;
+using NUnit.Framework;
+
+namespace Tests.Containers
+{
+    [TestFixture]
+    public class FuncChainTests
+    {
+
+        [Test]
+        public void Ordering()
+        {
+            var chain = FuncChain<int>.Wrap(2).Fmap(x => x + 10).Fmap(x => x * 10);
+            Assert.AreEqual((2 + 10) * 10, chain.Run());
+        }
+    }
+}
